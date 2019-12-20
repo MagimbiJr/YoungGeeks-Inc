f1c34a69e4404b1d780d99c1de7eb0b9ef5afa08
magimbijr <charleatanasi@gmail.com>
2019-12-20 22:40:11 +0300

    Solution Section

---
+++ src/components/Solution/Solution.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Solution.css';
+
+const Solution = props => (
+    <section className="solution">
+        <div className="container">
+            <div className="solution_wrapper">
+                <div className="solution_head">
+                    <h4>Solution</h4>
+                    <h2>We are here to relevance is irrelevant once the consumer chooses for you</h2>
+                    <button className="solution_btn">Start my Free Trial</button>
+                </div>
+                <div className="solution_content">
+                    <div className="solution-cont_item">
+                       <i className="fab fa-yelp"></i>
+                       <h3>Social Media Integration</h3>
+                       <p>In the email marketing quotes Hall of fame only the worthiest, deepest, truest and soundbite-iest are considered. Read them, Share them</p>
+                    </div>
+                    <div className="solution-cont_item">
+                       <i className="fas fa-user-secret"></i>
+                       <h3>Personalized Service</h3>
+                       <p>In the email marketing quotes Hall of fame only the worthiest, deepest, truest and soundbite-iest are considered. Read them, Share them</p>
+                    </div>
+                </div>
+                <div className="solution_content">    
+                    <div className="solution-cont_item">
+                       <i className="fas fa-chart-bar"></i>
+                       <h3>Advanced Reporting</h3>
+                       <p>In the email marketing quotes Hall of fame only the worthiest, deepest, truest and soundbite-iest are considered. Read them, Share them</p>
+                    </div>
+                    <div className="solution-cont_item">
+                       <i className="fab fa-free-code-camp"></i>
+                       <h3>Email Segmentation</h3>
+                       <p>In the email marketing quotes Hall of fame only the worthiest, deepest, truest and soundbite-iest are considered. Read them, Share them</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+)
+
+export default Solution
